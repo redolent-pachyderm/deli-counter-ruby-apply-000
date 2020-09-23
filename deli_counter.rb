53889b449f427600c66bc7738eee356c4dd771eb
Parker Florian <prkrrflorian@gmail.com>
2020-09-23 23:37:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,20 +1,20 @@
 # Write your code here.
-katz_deli = []   #there is a deli, an empty deli
+katz_deli = []       #there is a deli, an empty deli
 
-def line(katz_deli)  #queue in empty deli
-  if katz_deli.length >= 0  #if queue in deli is empty
-  puts "The line is currently empty."  #output displayed on screen
-else  #otherwise
-    line = [["Ada", "Gracie", "Kent"], ["1", "2", "3"]]  #redefine deli queue to patron , placement specifics
+def line(katz_deli)        #queue in empty deli
+  if katz_deli.length >= 0      #if queue in deli is empty
+  puts "The line is currently empty."     #output displayed on screen
+else        #otherwise
+    line = [["Ada", "Gracie", "Kent"], ["1", "2", "3"]]     #redefine deli queue to patron , placement specifics
       
-        katz_deli.each do |name, position|  #deli queue matches patron and placement
-          puts "Welcome, #{name}, you are number #{position} in line."  #output displayed on screen for patrons viewing
+        katz_deli.each do |name, position|       #deli queue matches patron and placement
+          puts "Welcome, #{name}, you are number #{position} in line."        #output displayed on screen for patrons viewing
           
     
-    take_a_number(katz_deli)  #current patrons and new patron 
-        line[0] << "Matz"
+    take_a_number(katz_deli)       #current patrons and new patron 
+    
+        katz_deli.line[0] << "Matz"
         
-        counter = 0 
         
           
             #line[0] << "Matz"
